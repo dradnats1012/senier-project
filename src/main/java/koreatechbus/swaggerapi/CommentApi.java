ae021b53f91263edde38b761247cbf67e4766e25
dradnats1012 <herjebi1012@gmail.com>
2024-05-02 22:50:15 +0900

    feat: 댓글 스웨거 추가

---
+++ src/main/java/koreatechbus/swaggerapi/CommentApi.java
@@ -0,0 +1,27 @@
+package koreatechbus.swaggerapi;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import koreatechbus.dto.comment.RequestCommentDTO;
+import koreatechbus.dto.comment.ResponseCommentDTO;
+
+@Tag(name = "댓글 api", description = "댓글 api")
+public interface CommentApi {
+
+    @Operation(summary = "댓글쓰기")
+    @PostMapping
+    ResponseEntity<Void> newComment(
+        @RequestBody RequestCommentDTO requestCommentDTO
+    );
+
+    @Operation(summary = "댓글 불러오기")
+    @GetMapping
+    ResponseEntity<List<ResponseCommentDTO>> getCommentByPostId();
+}
