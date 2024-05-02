fca01271e70f4600a0ac9ea70348c5e68341a813
dradnats1012 <herjebi1012@gmail.com>
2024-05-02 15:18:36 +0900

    feat: Comment Entity 클래스 추가

---
+++ src/main/java/koreatechbus/domain/Comment.java
@@ -0,0 +1,55 @@
+package koreatechbus.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Table(name = "comment")
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @Lob
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "post_time")
+    private String postTime;
+
+    @Column(name = "anonymous")
+    private Boolean anonymous;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public Comment(String content, String postTime, Boolean anonymous, Post post, User user) {
+        this.content = content;
+        this.postTime = postTime;
+        this.anonymous = anonymous;
+        this.post = post;
+        this.user = user;
+    }
+
+    public Comment() {
+
+    }
+}
