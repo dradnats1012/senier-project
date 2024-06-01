e3aceebd72c6e52702e08ca3d2b13ebf8e54786e
dradnats1012 <herjebi1012@gmail.com>
2024-06-02 00:16:03 +0900

    feat : SeatRepository 생성

---
+++ src/main/java/koreatechbus/repository/SeatRepository.java
@@ -0,0 +1,22 @@
+package koreatechbus.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import koreatechbus.domain.Bus;
+import koreatechbus.domain.Seat;
+import koreatechbus.domain.User;
+
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    boolean existsByBusAndSeatNum(Bus bus, Long seatNum);
+
+    boolean existsByUser(User user);
+
+    Seat findByBusAndSeatNum(Bus bus, Long seatNum);
+
+    List<Seat> findAllByBus(Bus bus);
+
+    Seat findBySeatId(Long seatId);
+}
